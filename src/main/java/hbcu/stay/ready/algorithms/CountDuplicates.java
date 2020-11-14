8b0881ddfb6142a581cf1d8f1b0e85fadae64cdf
Linda <lindaaohara@gmail.com>
2020-11-13 22:05:28 -0500

    Comfortable with Integer but not sure about String Test

--- src/main/java/hbcu/stay/ready/algorithms/CountDuplicates.java
+++ src/main/java/hbcu/stay/ready/algorithms/CountDuplicates.java
@@ -1,21 +1,51 @@
 package hbcu.stay.ready.algorithms;
 
+import java.util.Arrays;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
+     *
      * @param input
      * @return
      */
-    public Integer countDuplicates(String[] input){
-        return null;
+    public Integer countDuplicates(String[] input) {
+        Arrays.sort(input);
+        String currentValue = null;
+        int duplicates = 0;
+        for (int i = 0; i < input.length-1; i++) {
+            if (input[i] == input[i + 1] && input[i] != currentValue) {
+                duplicates ++;
+                currentValue = input[i];
+            }
+        }
+        return duplicates;
     }
+
 
     /**
      * Example of overloading a function that accepts a Array of Integers
+     *
      * @param input
      * @return
      */
-    public Integer countDuplicates(Integer[] input){
-        return null;
+    public Integer countDuplicates(Integer[] input) {
+        Arrays.sort(input);
+        Integer currentValue = null;
+        int duplicates = 0;
+        for (int i = 0; i < input.length-1; i++) {
+            if (input[i] == input[i + 1] && input[i] != currentValue) {
+                duplicates ++;
+                currentValue = input[i];
+            }
+        }
+        return duplicates;
     }
 }
+
+
+
+
+
+
+
